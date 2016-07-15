a7ac21726773870cdf9e8f8932f1178889b267ac
kde <kde@kde.dom>
2016-07-15 21:47:38 +0300

    15

---
+++ src/pag_m2/M2s.java
@@ -0,0 +1,18 @@
+package pag_m2;
+
+/**
+ * Массивы, Массив с динамичной инициализацией и индексацией
+ * 
+ * 
+ */
+public class M2s {
+
+    public static void main(String args[]) {
+        int[] m = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        for (int i = 0; i < m.length; i++) {
+            System.out.println("m[" + i + "]= " + m[i]);
+        }
+
+    }
+
+}
---
+++ src/pag_m4/M4s.java
@@ -0,0 +1,19 @@
+package pag_m4;
+
+/**
+ * Массивы, строковые массивы
+ *
+ */
+public class M4s {
+
+    public static void main(String args[]) {
+        String[] m = new String[3];
+        m[0] = "ноль";
+        m[1] = "один";
+        m[2] = "два";
+        System.out.println("m[0]=" + m[0]);
+        System.out.println("m[1]=" + m[1]);
+        System.out.println("m[2]=" + m[2]);
+
+    }
+}
