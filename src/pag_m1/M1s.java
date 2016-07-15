3a226d243c732d28fbce3b2a61d5cfca9089c344
kde <kde@kde.dom>
2016-07-15 21:43:34 +0300

    15

---
+++ src/pag_m1/M1s.java
@@ -0,0 +1,17 @@
+package pag_m1;
+
+/**
+ * Массивы,ключевое слово lenght, указывает количество элементов в массиве<br>
+ * имя массива.lenght
+ */
+public class M1s {
+
+    public static void main(String[] args) {
+        int[] m = new int[10];
+        int[] m1 = {0, 1, 2, 3, -7}; //динамический массив
+        int[] m2 = {0, 1, 2, 3, 4, 6};
+        System.out.println("элементов в массиве m[] m.length = " + m.length);
+        System.out.println("элементов в массиве m1[] m1.length = " + m1.length);
+        System.out.println("элементов в массиве m2[] m2.length = " + m2.length);
+    }
+}
